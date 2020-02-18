8c1d71076f1960e071961a1527263b0805797478
Philip Smith <philip.smith.k@gmail.com>
2020-02-18 15:56:54 -0500

    completed first draft

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const jumboTron = document.querySelector('.jumbotron');
 const navbar = document.querySelector('.navbar');
 
 const options = { 
-     rootMargin: "-400px 0px 0px 0px"
+     rootMargin: "-500px 0px 0px 0px"
 };
 const navObserver = new IntersectionObserver(navScroll, options);
 function navScroll(entries, navObserver) {
      entries.forEach(entry => {
           if (!entry.isIntersecting) {
-               console.log(entry);
                navbar.classList.add('scrolled');
           }
           else {
@@ -17,3 +16,13 @@ function navScroll(entries, navObserver) {
      });
 }
 navObserver.observe(jumboTron);
+
+const card = document.querySelectorAll('.card');
+
+card.forEach(c => c.addEventListener("mouseover", function(){
+     cardBody = c.querySelector('.card-body');
+     if(!cardBody.classList.contains('.cardbody-hover')){
+          cardBody.classList.add('.cardbody-hover');
+          console.log("hey");
+     }
+}));

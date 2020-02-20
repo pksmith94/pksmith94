e88ca13856936d416146ed4420414fd7489b09bd
Philip Smith <philip.smith.k@gmail.com>
2020-02-19 22:13:30 -0500

    implemented javascript

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function navScroll(entries, navObserver) {
      });
 }
 navObserver.observe(jumboTron);
+
+const triggers = document.querySelectorAll('.nav-link');
+const highlight = document.createElement('span');
+document.body.append(highlight);
+highlight.classList.add('highlight');
+
+function highlightNavLink() {
+      const linkCoords = this.getBoundingClientRect();
+      const coords = {
+           width: linkCoords.width - 9,
+           height: linkCoords.height,
+           top: linkCoords.top + window.scrollY,
+           left: linkCoords.left + window.scrollX
+      };
+
+     highlight.style.width = `${coords.width}px`;
+     highlight.style.height = `${coords.height}px`;
+     highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+
+
+}
+
+triggers.forEach(c => c.addEventListener('mouseenter', highlightNavLink));

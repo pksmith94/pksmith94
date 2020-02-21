6f62c9cfef017cfbec578d5044194eddacb0c7c6
Philip Smith <philip.smith.k@gmail.com>
2020-02-21 17:56:10 -0500

    adjustment to navbar animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const jumboTron = document.querySelector('.jumbotron');
 const navbar = document.querySelector('.navbar');
 
 const options = { 
-     rootMargin: "-500px 0px 0px 0px"
+     rootMargin: "-600px 0px 0px 0px"
 };
 const navObserver = new IntersectionObserver(navScroll, options);
 function navScroll(entries, navObserver) {
